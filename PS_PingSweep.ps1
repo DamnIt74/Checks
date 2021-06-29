c4e8e390dfb4dbc3505d597a2f1753cb469f3d46
Damn_It <67222978+DamnIt74@users.noreply.github.com>
2021-06-30 09:52:34 +1200

    Delete PS_PingSweep.ps1

--- PS_PingSweep.ps1
+++
@@ -1,7 +0,0 @@
-for ($i=1; $i -lt 255; $i++)
-{
-	$pingy=New-Object System.Net.Networkinformation.ping
-	$hosty="192.168.2.$i"
-	$pingx=$pingy.Send($hosty, 1)
-	if($pingx.Status -eq "Success"){$hosty}
-}
